04f9fb0d5f9ffd8e20db39ee8aea66cfe4c1e754
kusuri12 <waterfail12@naver.com>
2025-06-19 10:25:43 +0900

    print_result 함수에 변수 추가
    나도 수행은 해야지

--- print_result.c
+++ print_result.c
@@ -2,22 +2,28 @@
 #include "func.h"
 
 int f_1302_print_result(char user_guess[], char result[]) {
+    int is_correct = 1;
+
+    // 사용자가 추측한 입력 값 출력
     printf("추측: ");
     for (int i = 0; i < WORD_LENGTH; i++) {
         printf("%c ", user_guess[i]);
     }
 
+    // 판정 결과 출력(O, ~, X)
     printf("\n결과: ");
     for (int i = 0; i < WORD_LENGTH; i++) {
         printf("%c ", result[i]);
     }
     printf("\n\n");
 
+    // 모든 문자가 정답('O')인지 확인하여 정답 여부 반환
     for (int i = 0; i < WORD_LENGTH; i++) {
         if (result[i] != 'o') {
-            return 0;
+            is_correct = 0;
+            return is_correct; // 아직 틀린 글자 있음
         }
     }
 
-    return 1;
+    return is_correct; // 모든 글자 정답
 }
